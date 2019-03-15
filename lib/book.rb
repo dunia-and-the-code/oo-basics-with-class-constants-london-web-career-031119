9abd5d59143c1c2ecf5e631317d30faf1bb640e6
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-15 17:30:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :book
+  attr_reader :title
 
-  def initialize(book)
-    @book = book
+  GENRE = []
+
+  def initialize(title)
+    @title = title
   end
 
   def turn_page
